312fc49b3346533f3af4c9a4bfadee4f7416c79f
thiagork9 <thiagosantostc10@gmail.com>
2024-02-12 17:02:32 -0300

    comitando primeira versão memoteca

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { ExcluirPensamentosComponent } from './componentes/pensamentos/excluir-pensamentos/excluir-pensamentos.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
+
+const routes: Routes = [
+  {
+      path: '',
+      redirectTo: 'listarPensamento',
+      pathMatch: 'full'
+  },
+  {
+      path: 'criarPensamento',
+      component: CriarPensamentoComponent
+  },
+  {
+      path: 'listarPensamento',
+      component: ListarPensamentoComponent
+  },
+  {
+      path: 'pensamentos/excluirPensamento/:id',
+      component: ExcluirPensamentosComponent
+  },
+  {
+    path: 'pensamentos/editarPensamento/:id',
+    component: EditarPensamentoComponent
+}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  @Input() pensamento: Pensamento = {
+    id:0,
+    conteudo: 'Testando funcionamento do conteudo do card',
+    autoria: 'Dev Thiago',
+    modelo: 'modelo3'
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+    larguraPensamento(): string {
+      if(this.pensamento.conteudo.length >= 256 ){
+        return 'pensamento-g'
+      }
+        return 'pensanmento-p'
+    }
+}
